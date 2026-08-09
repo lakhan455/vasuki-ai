e4297f20cc2ede180e70d28e58934c9fe9742092
lakhan455 <lakhhanprajapat@gmail.com>
2026-08-09 22:45:08 +0530

    Fix GitHub CI for backend imports frontend env and ESLint

---
+++ frontend/eslint.config.mjs
@@ -0,0 +1,27 @@
+﻿import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
+
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+
+  {
+    rules: {
+      "@next/next/no-html-link-for-pages": "off",
+      "react-hooks/set-state-in-effect": "off",
+      "react-hooks/exhaustive-deps": "warn",
+      "@next/next/no-img-element": "warn",
+      "@typescript-eslint/no-unused-vars": "warn",
+    },
+  },
+
+  globalIgnores([
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+  ]),
+]);
+
+export default eslintConfig;
